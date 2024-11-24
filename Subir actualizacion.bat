f71e879e1ba8334166c3bcceb6ecbe04c0e337bc
Redkaku <mailov.537@gmail.com>
2024-11-24 15:49:14 -0600

    Actualizacion de niveles y  puntaje final

--- Subir actualizacion.bat
+++ Subir actualizacion.bat
@@ -2,14 +2,14 @@
 chcp 65001 > nul
 
 :: Crear y cambiar a la nueva rama BETA
-git checkout -b OdioEsto
+::git checkout -b master
 
 :: Agregar todos los archivos y hacer un commit inicial
 git add .
 git commit -m "Actualizacion de niveles y  puntaje final"
 
 :: Subir la rama BETA al repositorio remoto
-git push -u origin OdioEsto
+git push -u origin master
 
 :: Pausa para mostrar mensajes en consola
 pause
